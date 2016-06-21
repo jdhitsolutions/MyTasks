956fca49485b4e545d82b0305694ef9b962cf5f2
jdhitsolutions <jhicks@jdhitsolutions.com>
2016-06-21 10:36:56 -0400

    0.0.1

--- MyTasks.psm1
+++ MyTasks.psm1
@@ -11,10 +11,6 @@ $myTaskID = 0
 
 $myTasks = @()
 
-#endregion
-
-#region Definitions
-
 if (Test-Path -Path $myTaskCategory) {
     #dot source user list
     . $userTaskCategory
@@ -36,6 +32,9 @@ A class to define a task or to-do item
 #>
 
 #region Properties
+
+# ID and OverDue values are calcuated at run time.
+
 [int]$ID
 [string]$Name
 [string]$Description
@@ -58,22 +57,16 @@ hidden[guid]$TaskID = (New-Guid)
 [void]CompleteTask() {
     $this.Completed = $True
     $this.Progress = 100
+    $this.Overdue = $False
+    $this.TaskModified = Get-Date
 }
 
 hidden [void]Refresh() {
-  if ( (Get-Date) -gt $this.DueDate) {
-    $this.Overdue = $True
-  }
-  else {
-    $this.Overdue = $False
-  }
+  
+  if ((Get-Date) -gt $this.DueDate) {
+    $this.Overdue = $True 
+  } 
 
-  if ($this.Progress -eq 100) {
-    $this.Completed = $True
-  }
-
-  #update modified property
-  $this.TaskModified = Get-Date
 } #refresh
 
 
@@ -96,195 +89,13 @@ MyTask([string]$Name,[datetime]$DueDate,[string]$Description,[TaskCategory]$Cate
 
     $this.Refresh()
 }
+
 #endregion
 
 } #end class definition
 
-#endregion
-
-#endregion
-
-#region Function
-
-Function New-MyTask {
-
-[cmdletbinding()]
-Param(
-[Parameter(
-Position = 0, 
-Mandatory,
-HelpMessage="Enter the name of your task",
-ValueFromPipelineByPropertyName
-)]
-[string]$Name,
-
-[Parameter(Mandatory,
-HelpMessage = "Select a task category",
-ValueFromPipelineByPropertyName
-)]
-[ValidateNotNullorEmpty()]
-[TaskCategory]$Category,
-
-[Parameter(ValueFromPipelineByPropertyName)]
-[ValidateNotNullorEmpty()]
-[dateTime]$DueDate = (Get-Date).AddDays(7),
-
-[Parameter(ValueFromPipelineByPropertyName)]
-[string]$Description,
-
-[switch]$Passthru
-)
-
-$task = New-Object -TypeName MyTask -ArgumentList $Name,$DueDate,$Description,$Category
-
-$myTaskID++
-$task.id = $myTaskID
-
-#add task to array
-$global:myTasks+=$task
-
-#convert to xml
-$newXML = $task | Select *,TaskCreated,TaskModified,TaskID,Completed -ExcludeProperty ID | ConvertTo-Xml
-
-#add task to disk via XML file
-if (Test-Path -Path $mytaskPath) {
-
-    #import xml file
-    [xml]$in = Get-Content -Path $mytaskPath
-
-    #check if TaskID already exists in file and skip
-    $id = $task.TaskID
-    $result = $in | Select-XML -XPath "//Object/Property[text()='$id']"
-    if (-Not $result.node) {
-        #if not,import node
-        $imp = $in.ImportNode($newXML.objects.object,$true)
-
-        #append node
-        $in.Objects.AppendChild($imp) | Out-Null
-        #update file
-
-        $in.Save($mytaskPath)
-    }
-    else {
-        Write-Verbose "Skipping $id"
-    }
-}
-else {
-    #If file doesn't exist create task and save to a file
-    $newxml.Save($mytaskPath)
-}
-
-If ($Passthru) {
-    $task
-}
-
-} #New-MyTask
-
-Function Set-MyTask {
-
-[cmdletbinding(SupportsShouldProcess)]
-Param (
-[Parameter(ValueFromPipeline)]
-[MyTask]$Task,
-[string]$Name,
-[string]$Description,
-[datetime]$DueDate,
-[ValidateRange(0,100)]
-[int]$Progress,
-[TaskCategory]$Category
-
-)
-
-Begin {
-    Write-Verbose "[BEGIN  ] Starting: $($MyInvocation.Mycommand)"  
-    #display PSBoundparameters formatted nicely for Verbose output  
-    [string]$pb = ($PSBoundParameters | format-table -AutoSize | Out-String).TrimEnd()
-    Write-Verbose "[BEGIN  ] PSBoundparameters: `n$($pb.split("`n").Foreach({"$("`t"*4)$_"}) | out-string) `n" 
-
-    $PSBoundParameters.Remove("Verbose") | Out-Null
- } #begin
-
-Process {
-    Write-Verbose "[PROGRESS] Updating task $($task.Name)"
-    #remove Task object from PSBoundParameters
-    $PSBoundParameters.Remove("Task") | Out-Null   
-    $PSBoundParameters.keys | foreach {
-      Write-Verbose "[PROGRESS] Updating $_ = $($psboundParameters.item($_))"
-
-      $task.$($_) = $psboundParameters.item($_)
-    }
-    #update object
-    $task.Refresh()
-
-    #update source
-
-    #pass object to the pipeline
-    $task
-
-} #process
-
-
-End {
-    Write-Verbose "[END    ] Ending: $($MyInvocation.Mycommand)"
-} #end
-} #Set-MyTask
-
-Function Remove-MyTask {
-
-} #Remove-MyTask
-
-Function Get-MyTask {
-[cmdletbinding()]
-Param(
-[string]$Name,
-[switch]$All
-)
-
-} #Get-MyTask
-
-Function Show-MyTask {
-[cmdletbinding()]
-Param([switch]$All)
-
-#run Get-MyTask
-
-#colorize output using Write-Host
-
-} #Show-MyTask
-
-
-#this is a private function to the module
-Function _ImportTask {
-[cmdletbinding()]
-Param([string]$Path = $myTaskpath)
-
-[xml]$In = Get-Content -Path $Path
-
-foreach ($obj in $in.Objects.object) {
-  $obj.Property | foreach -Begin {$propHash = [ordered]@{}} -Process {
-    $propHash.Add($_.name,$_.'#text')
-  } 
-
-
-  Try {
-  $tmp = New-Object -TypeName MyTask -ArgumentList $propHash.Name, $propHash.DueDate, $propHash.Description, $propHash.Category
-  $tmp.TaskID = $prophash.TaskID
-  $tmp.Progress = $prophash.Progress
-  $tmp.TaskCreated = $prophash.TaskCreated
-  $tmp.TaskModified = $prophash.TaskModified
-  $tmp.Completed = $prophash.Completed
-
-  $tmp
-  }
-  Catch {
-    Write-Error $_
-  }
-
-}
-
-} #_ImportTask
-
-#endregion
+#dot source functions
+. $psscriptroot\myTasksFunctions.ps1
 
 #if existing task XML file is found, import it and create MyTask objects
 
