99bf57fee357fd7d16bd742418aa5480e98841bc
jdhitsolutions <jhicks@jdhitsolutions.com>
2016-08-25 18:03:56 -0400

    v1.0.1 Added Pester tests fixed a bug with exported variables

--- Tests/MyTasks.tests.ps1
+++ Tests/MyTasks.tests.ps1
@@ -1,9 +1,142 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. $here\..\$sut
 
-Describe MyTasks {
-    It "does something useful" {
-        $true | Should Be $true
+
+import-module ..\Mytasks -Force
+
+InModuleScope MyTasks {
+
+
+Describe "The module" {
+    It "Should have 11 functions" {
+        (get-module -name mytasks).exportedfunctions.count | should be 11
     }
+
+    It "Should have 7 aliases command" {
+        (Get-command -Module MyTasks -commandtype Alias).count | should be 7
+    }
+
+    It "Should export 3 variables by default" {
+        (Get-Module -Name Mytasks).ExportedVariables.Count | Should be 3
+    }
+
+    It "Should have a formatting xml file" {
+        (Get-Module -Name Mytasks).ExportedFormatFiles.Count | Should be 1
+    }
+} 
+
+
+Describe Categories {
+
+$myTaskCategory = "TestDrive:\myTaskCategory.txt"
+
+    It "Default categories are Work,Personal,Other, and Customer" {
+       $myTaskDefaultCategories.count | Should be 4
+       $myTaskDefaultCategories -join "-"| Should Match "Work"
+       $myTaskDefaultCategories -join "-"| Should Match "Personal"
+       $myTaskDefaultCategories -join "-"| Should Match "Other"
+       $myTaskDefaultCategories -join "-"| Should Match "Customer"
+    }
+
+    It "Adds a Testing category to $myTaskCategory" {
+        Add-MyTaskCategory -Category Testing
+        $c =  Get-Content -path $myTaskCategory -raw
+        Test-Path -path $myTaskCategory | Should Be $True
+        $c | Should match "Testing"
+    }
+
+    It "Adds multiple categories" {
+        Add-MyTaskCategory -Category Work,Personal,Other,Training,Demo
+        (Get-MyTaskCategory).count| Should be 6
+    }
+
+    It "Can remove a category" {
+        Remove-MyTaskCategory -Category Demo
+         (Get-MyTaskCategory).count| Should be 5
+    }
+
 }
+
+Describe Tasks {
+
+Mock Get-Date { return ("9/1/2016 12:01:00PM" -as [datetime])  }
+#
+
+#need absolute path
+$mytaskPath = Join-Path $TestDrive -child "myTasks.xml" 
+$myTaskArchivePath = Join-Path -Path $TestDrive -ChildPath myTasksArchive.xml
+
+$myTaskCategory = "TestDrive:\myTaskCategory.txt"
+
+Add-MyTaskCategory -Category Work,Personal,Other,Training,Testing
+
+    It "Should create a new task" {
+        $a = New-MyTask -Name Test1 -DueDate 12/31/2016 -Category Testing -Passthru
+        $a.id | Should be 0
+        Test-Path $mytaskPath | Should Be $True
+    }
+
+    It "Should create a new task with a 7 day default due date" {
+        $b = New-MyTask -Name Test2 -Category Testing -Passthru
+        $b.DueDate | Should Be "09/08/2016 12:01:00"
+    }
+
+    It "Should get tasks" {
+        $t = Get-MyTask -name Test1
+        $t.ID | Should be 2
+        $t.name | Should be "Test1"
+        $t.duedate | Should match "12/31/2016"
+        $t.Category | Should be "Testing"
+        $t.OverDue | Should Be $False
+    }
+
+    It "Should modify a task by name" {
+        Set-MyTask -Name "Test1" -Progress 50 -Description "Pester Test" -DueDate "8/1/2016"
+         $t = Get-MyTask -name Test1
+         $t.Progress | should be 50
+         $t.description | should be "Pester Test"
+         $t.duedate | Should be ("8/1/2016" -as [datetime])
+         $t.OverDue | Should be $true
+    }
+
+    It "Should modify a task via the pipeline" {
+        Get-MyTask -Name "Test1" | Set-MyTask -Progress 80 
+         $t = Get-MyTask -name  Test1
+         $t.Progress | should be 80
+    }
+
+    #add some other tasks
+       New-MyTask -Name A -DueDate 12/31/2016 -Category Testing
+       New-MyTask -Name B -DueDate 10/01/2016 -Category Work
+       New-MyTask -Name C -DueDate 11/11/2016 -Category Personal
+
+    It "Should Complete a task" {
+
+        {Complete-Mytask -Name Test1 -ErrorAction Stop} | Should Not Throw
+        (Get-MyTask | Measure-Object).Count | Should be 4
+        (Get-MyTask -Completed | Measure-Object).Count | Should be 1
+
+    }
+
+    It "Should archive or save a task" {
+        $save = Join-path $TestDrive -ChildPath "Archive.xml"
+        Get-MyTask -Completed | Save-MyTask -Path $save
+        Test-Path $save | should Be $True
+        Get-MyTask -Name Test1 | Should Be $null
+        (Get-MyTask).count | Should be 4
+    }
+
+    It "Should remove a task and backup the task file" {
+
+        Mock Get-Date { return "20160901"  } -ParameterFilter {format -eq "yyyyMMdd"} 
+        $home = $TestDrive
+        mkdir $home\documents
+    
+
+        {Remove-myTask -Name A} | Should not Throw
+        {Get-MyTask -Name B | Remove-MyTask } | should not Throw
+        (Get-MyTask).count | Should be 2
+        Test-Path $TestDrive\documents\MyTasks_Backup_20160901.xml | Should be $True
+    }
+
+}
+
+} #in module
